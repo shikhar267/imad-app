66614a4d4721f062a1fc0945ad434ca92931b8a2
shikhar267 <shikharsingh267@gmail.com>
2018-02-23 17:07:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title: 'Article One |shikhar singh',
+    heading: 'Article One',
+    date: 'Sep 5,2016',
+    content: `
+    <p>This is shikhar singh</p>
+  <p>  Graduating</p>
+   <p> Learner </p> `
+     
+};
+var htmlTemplate=`
+<html>
+    <head>
+        <title>${title}</title>
+      
+            <meta name="viewport" content="width-device-width, initial-scale-1" />
+           <link href="ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        
+                 <div class="container">
+                   <div>
+                       <a href="/">Home</a>
+                   </div>
+                   <hr>
+                   <h3>${heading}</h3>
+                   <div>
+                      ${date}
+                   </div>
+                   <div>
+                     ${content}
+                   </div>
+                   
+            </div>
+    </body>
+</html>
+
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
