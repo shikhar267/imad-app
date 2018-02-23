da39f9428b71bd8fe8d1eb564b98b509fa02dbaa
shikhar267 <shikharsingh267@gmail.com>
2018-02-23 18:55:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,10 @@
 console.log('Loaded!');
 
 var element=document.getElementById("main-text");
-element.innerHTML=" NEw value"
+element.innerHTML=" NEw value";
+
+
+var element=document.getElementById("img");
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
