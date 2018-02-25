9dad5a86a77289b21efa827d823a8d96447e98d9
shikhar267 <shikharsingh267@gmail.com>
2018-02-25 19:09:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,28 @@ button.onclick=function(){
     request.open("GET","http://shikharsingh267.imad.hasura-app.io/counter",true);
     request.send(null);
 };
+
+   
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
+               var names=request.responseText;
+               names=JSON.parse(names);
+     var list='';
+     for(var i=0;i<names.length;i++){
+         list+='<li>'+names[i]+'</li>';
+     }
+     var ul=document.getElementById('namelist');
+     ul.innerHTML=list;
+            }
+        }
+    };
+    //make the request
+    
+    request.open("GET","http://shikharsingh267.imad.hasura-app.io/submit-name"+ name,true);
+    request.send(null);
+    
  var nameInput=document.getElementById('name');
  var name=nameInput.value;
  var submit=document.getElementById("submit_btn");
