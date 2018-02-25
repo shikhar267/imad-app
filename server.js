29a0ac5d2943ff00568f6abdb667b91c480c0864
shikhar267 <shikharsingh267@gmail.com>
2018-02-25 18:25:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/ui/madi.png', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names=[];
-app.get('/submit-name/',function(req,res){
+app.get('/submit-name',function(req,res){
     var name=req.query.name;
     names.push(name);
     //JSON JAVASCRIPT OBJECT NOTATION
