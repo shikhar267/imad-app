fc5c77090b89edfdbb2b395397697e66354e8a7a
shikhar267 <shikharsingh267@gmail.com>
2018-03-01 21:07:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 var config={
     user: 'shikharsingh267',
     database: 'shikharsingh267',
@@ -12,7 +13,7 @@ var config={
 }; 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 function createTemplate(data){
     var title=data.title;
@@ -68,7 +69,20 @@ app.get('/hash/:input',function(req,res){
     var hashedString=hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
 });
-
+app.post('/create-user',function(req,res){
+    
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.RandomBytes(128).toString('hex');
+   var dbString=hash(password,salt); 
+   pool.query('INSERT INTO "user"(username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err){
+        res.status(500).send(err.toString());}
+        else{
+            res.send("User successfully created "+username);
+        }
+   });
+});
 
 
 
