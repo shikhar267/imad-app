d110e9a90dd9a7b0c377075b4abf3dfe8652238a
shikhar267 <shikharsingh267@gmail.com>
2018-03-01 19:55:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user: 'shikharsingh267',
     database: 'shikharsingh267',
@@ -57,6 +58,23 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+function hash(input,salt){
+    //how do wecreate hash
+    var hashed=crypto.pbkdf2Sync(input,salt,512,'sha512');
+    return hashed.toString('hex');
+    
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+});
+
+
+
+
+
+
+
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
